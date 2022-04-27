844a44c2785c182e2cdc568c1b12cdc43385818b
Suresh2629 <97591413+Suresh2629@users.noreply.github.com>
2022-04-27 18:18:49 +0530

    Number = int(input()) Temp = Number Sum = 0 prod = 1
    while Temp > 0:
        lastDigit = Temp % 10
        Sum = Sum + lastDigit
        prod = prod * lastDigit
        Temp = Temp // 10
    
    
    if Sum == prod:
        print("Spy Number")
    else:
        print("Not Spy Number")

---
+++ Spy_Number.py
@@ -0,0 +1,16 @@
+Number = int(input())
+Temp = Number
+Sum = 0
+prod = 1
+
+while Temp > 0:
+    lastDigit = Temp % 10
+    Sum = Sum + lastDigit
+    prod = prod * lastDigit
+    Temp = Temp // 10
+
+
+if Sum == prod:
+    print("Spy Number")
+else:
+    print("Not Spy Number")
