6b48ca60c1859e9d73eb8188516c05660e2e52a6
Suresh2629 <97591413+Suresh2629@users.noreply.github.com>
2022-06-28 22:12:41 +0530

    p=int(input()) l=list(map(int,input().split())) n=[] k=[] res=[] for i in l:     if i%2!=0:         n.append(i)     else:         k.append(i) res=n+k for j in res:     print(j,end=' ')

---
+++ array_of_add_and_even_elements.py
@@ -0,0 +1,13 @@
+p=int(input())
+l=list(map(int,input().split()))
+n=[]
+k=[]
+res=[]
+for i in l:
+    if i%2!=0:
+        n.append(i)
+    else:
+        k.append(i)
+res=n+k
+for j in res:
+    print(j,end=' ')
